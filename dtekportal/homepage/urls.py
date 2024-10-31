52f16fba6e1c57c54608a3a1846b15b43f9c41e4
pingu <nor@acorneroftheweb.com>
2024-10-31 15:38:37 +0100

    Add another page with documents related to the division

--- dtekportal/homepage/urls.py
+++ dtekportal/homepage/urls.py
@@ -10,6 +10,7 @@ app_name = 'homepage'
 urlpatterns = [
     path("", TemplateView.as_view(template_name='homepage/index/index.html'), name='index'),
     path("newsletter", TemplateView.as_view(template_name='homepage/newsletter.html'), name='newsletter'),
+    path("documents", TemplateView.as_view(template_name='homepage/documents.html'), name='documents'),
     path('studiesocial-hjalp', RedirectView.as_view(url=static('homepage/studiesociala-natverket.pdf')), name='studiesocial-hjalp'),
 ]
 
